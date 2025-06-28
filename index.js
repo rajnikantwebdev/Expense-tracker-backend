d409c92b830350af18985f77ee295359fbc0ca13
rajnikantwebdev <rajnikantdevep@gmail.com>
2025-06-28 07:29:27 +0530

    "/" route added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(cors())
 app.use(express.json())
 app.use("/api", expenseRoutes)
 
+app.get("/", (req, res) => {
+    res.send({message: "connected successfully!"})
+})
+
 app.listen(5000, () => {
     console.log(`server is listening on port 5000`)
 })
